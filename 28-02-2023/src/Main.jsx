bcf37a2ca98e8258fa64d9a9f2874b16f78701aa
Adriana Origlio <121047388+chibiusagitsukino@users.noreply.github.com>
2023-02-28 17:21:09 +0100

    Add files via upload

---
+++ 28-02-2023/src/Main.jsx
@@ -0,0 +1,16 @@
+import Header from './components/header'
+import Body from './components/body'
+import Footer from './components/footer'
+import './main.css'
+
+const Main = () => {
+  return (
+    <div className='Main'>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default Main
---
+++ 28-02-2023/src/components/body/Body.jsx
@@ -0,0 +1,9 @@
+const Body = () => {
+  return (
+    <body>
+      <div className='Body'></div>
+    </body>
+  )
+}
+
+export default Body
